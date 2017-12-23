82b37973e9dc3bbe6adee5bdc03295c8145330ec
antastsy <664692054@qq.com>
2017-12-23 08:35:11 +0800

    add easy/hard selection

--- src/menu.py
+++ src/menu.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import cocos
-import define
+import pyglet
 from gluttonous2 import HelloWorld2
 from cocos.director import director
+from cocos.sprite import Sprite
 
 class Menu(cocos.layer.ColorLayer):
     is_event_handler = True
@@ -10,8 +11,41 @@ class Menu(cocos.layer.ColorLayer):
     def __init__(self):
         wid = director.get_window_size()[0]
         heg = director.get_window_size()[1]
-        super(Menu, self).__init__(255, 255, 255, 200, wid, heg)
-        self.text1 = cocos.text.Label('CHOOSE A WORDLIST:',
+        super(Menu, self).__init__(255,255,255,255)
+
+        self.text1 = Sprite('Choose List.png')
+        self.text1pos = self.text1.width / 2 + wid / 2 - 200, self.text1.height / 2 + heg / 2 + 150
+        self.text1.position = self.text1pos
+        self.add(self.text1, 1)
+
+        self.text2 = Sprite('CEM8 1.png')
+        self.text2pos = self.text2.width / 2 + wid / 2 - 180, self.text2.height / 2 + heg / 2 + 50
+        self.text2.position = self.text2pos
+        self.add(self.text2)
+
+        self.text3 = Sprite('CET4 1.png')
+        self.text3pos = self.text3.width / 2 + wid / 2 - 180, self.text3.height / 2 + heg / 2 - 50
+        self.text3.position = self.text3pos
+        self.add(self.text3)
+
+        self.text4 = Sprite('CET6 1.png')
+        self.text4pos = self.text4.width / 2 + wid / 2 - 180, self.text4.height / 2 + heg / 2 - 150
+        self.text4.position = self.text4pos
+        self.add(self.text4)
+
+        self.text5 = Sprite('Easy 1.png')
+        self.text5pos = self.text5.width / 2 + wid / 2 + 80, self.text5.height / 2 + heg / 2
+        self.text5.position = self.text5pos
+        self.add(self.text5)
+
+        self.text6 = Sprite('Hard 1.png')
+        self.text6pos = self.text6.width / 2 + wid / 2 + 80, self.text6.height / 2 + heg / 2 - 100
+        self.text6.position = self.text6pos
+        self.add(self.text6)
+
+        self.listselect = None
+        self.modeselect = None
+        '''self.text1 = cocos.text.Label('CHOOSE A WORDLIST:',
                                       font_name='times new roman',
                                       font_size=32,
                                       color=define.GRAY)
@@ -39,8 +73,8 @@ class Menu(cocos.layer.ColorLayer):
 
         self.text4.position = 273, 100
         self.add(self.text4, 99999)
-        self.i = 0
-
+        self.i = 0'''
+    '''
     def on_mouse_press(self, x, y, buttons, modifiers):
         if y>50 and y<150:
             self.i=1
@@ -55,4 +89,38 @@ class Menu(cocos.layer.ColorLayer):
             self.remove(self.text4)
             self.remove(self.text3)
             self.remove(self.text2)
-            self.color=(0,0,0)
+            self.color=(0,0,0)'''
+
+    def reset_list(self):
+        self.text2.image = pyglet.resource.image('CEM8 1.png')
+        self.text3.image = pyglet.resource.image('CET4 1.png')
+        self.text4.image = pyglet.resource.image('CET6 1.png')
+
+    def reset_mode(self):
+        self.text5.image = pyglet.resource.image('Easy 1.png')
+        self.text6.image = pyglet.resource.image('Hard 1.png')
+
+    def on_mouse_press(self, x, y, buttons, modifiers):
+
+         if x in range(160, 280):
+             self.reset_list()
+             if y in range(300, 330):
+                 self.listselect = 'CEM8'
+                 self.text2.image = pyglet.resource.image('CEM8 2.png')
+             elif y in range(200, 230):
+                 self.listselect = 'CET4'
+                 self.text3.image = pyglet.resource.image('CET4 2.png')
+             elif y in range(100, 130):
+                 self.listselect = 'CET6'
+                 self.text4.image = pyglet.resource.image('CET6 2.png')
+         elif x in range(420, 500):
+             self.reset_mode()
+             if y in range(250, 280):
+                 self.modeselect = 'easy'
+                 self.text5.image = pyglet.resource.image('Easy 2.png')
+             elif y in range(150, 180):
+                 self.modeselect = 'hard'
+                 self.text6.image = pyglet.resource.image('Hard 2.png')
+         else:
+             return
+        
